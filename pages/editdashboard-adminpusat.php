8dbcf4a111fbfa7c2fdb39c7bdfeb2f04423602a
Maruf <99467481+Muhammad-Maruff@users.noreply.github.com>
2022-12-22 16:16:42 +0700

    edit dashboard

--- pages/editdashboard-adminpusat.php
+++ pages/editdashboard-adminpusat.php
@@ -8,15 +8,128 @@
   //buat koneksi
   $koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
 
+  //jika button simpan diklik
+  if(isset($_POST['btn-simpan'])){
+    if(isset($_GET['hal']) == "edit"){
+
+      $edit = mysqli_query($koneksi, "UPDATE tb_dashboard SET
+
+                                            upload_data = '$_POST[upload_data]',
+                                            upload_kpi = '$_POST[upload_kpi]',
+                                            inspirasi_kpi = '$_POST[inspirasi_kpi]',
+                                            approval = '$_POST[approval]'
+
+                                      WHERE id = '$_GET[id]'
+          ");
+
+          if($edit){
+            echo "<script>
+              alert('Data berhasil edit!');
+              document.location='dashboard-adminpusat.php'
+            </script>";
+          }
+          else{
+            echo "<script>
+              alert('Data gagal edit!');
+              document.location='dashboard-adminpusat.php'
+            </script>";
+          }
+    }
+    //Data akan disimpan
+
+    else{
+      $simpan = mysqli_query($koneksi, "INSERT INTO tb_dashboard (upload_data,upload_kpi,inspirasi_kpi,approval)
+      VALUE ( 
+              '$_POST[upload_data]',
+              '$_POST[upload_kpi]',
+              '$_POST[inspirasi_kpi]',
+              '$_POST[approval]'
+  ");
+
+//uji jika simpan data sukses
+if($simpan){
+echo "<script>
+alert('data berhasil disimpan!');
+document.location='dashboard-adminpusat.php';
+</script>";
+} else{
+echo "<script>
+alert('Simpan data gagal');
+document.location='dashboard-adminpusat.php';
+</script>";
+}
+    }
+
+    $tampil = mysqli_query($koneksi, "SELECT * FROM tb_dashboard order by id asc");
+            while($data = mysqli_fetch_array($tampil));
+
+  }
+
+  //deklarasi variabel untuk menampung data yang akan diedit
+  $vupload_data = "";
+  $vupload_kpi = "";
+  $vinspirasi_kpi = "";
+  $vapproval = "";
+  
+
+  //jika tombol edit diedit/hapus
+  if(isset($_GET['hal'])){
+    //jika edit data
+    if($_GET['hal'] == "edit"){
+      //tampilkan data yang akan diedit
+
+
+      $tampil=mysqli_query($koneksi, "SELECT * FROM tb_dashboard WHERE id = '$_GET[id]'");
+
+      $data = mysqli_fetch_array($tampil);
+      if($data){
+        //jika data ditemukan, maka data ditampung kedalam variabel
+
+        $vupload_data = $data['upload_data'];
+        $vupload_kpi = $data['upload_kpi'];
+        $vinspirasi_kpi = $data['inspirasi_kpi'];
+        $vapproval = $data['approval'];
+
+      }
+
+
+    }
+  }
+
 
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="../library/bootstrap-5/bootstrap.min.css" rel="stylesheet" />
+        <script src="../library/bootstrap-5/bootstrap.bundle.min.js"></script>
+        <script src="../library/autocomplete.js"></script>
+        <title>For-Pi | Edit Tipe Target</title>
+    </head>
+
+    <?php
+	session_start();
+
+	// cek apakah yang mengakses halaman ini sudah login
+	if($_SESSION['level'] != "admin pusat"){
+		header("location:../login.php?pesan=gagal");
+	}
+
+	?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>For-Pi | Juknis</title>
+  <title>For-Pi | Tipe Target</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -29,23 +142,12 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <link rel="stylesheet" href="../dist/css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-
-<?php
-session_start();
-	// cek apakah yang mengakses halaman ini sudah login
-	if($_SESSION['level'] != "admin pusat"){
-		header("location:../login.php?pesan=gagal");
-	}
-?>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-
- <!-- Preloader -->
- <div class="preloader flex-column justify-content-center align-items-center">
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__shake" src="../dist/img/Logo_PLNN.png" alt="PLNLOGO" height="60" width="60">
   </div>
 
@@ -60,7 +162,7 @@ session_start();
         <a href="admin-pusat.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="juknis-adminpusat.php" class="nav-link">Juknis</a>
+        <a href="tipe-target-adminpusat.php" class="nav-link">Tipe Target</a>
       </li>
     </ul>
 
@@ -88,7 +190,8 @@ session_start();
         </div>
       </li>
 
-      <!-- Messages Dropdown Menu -->
+
+      <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <?php
@@ -99,7 +202,7 @@ session_start();
           <div class="dropdown-divider"></div>
           <a href="keluar.php" class="dropdown-item">
           <i class="fa-solid fa-right-from-bracket"></i>
-          logout
+logout
           </a>
       </li>
     </ul>
@@ -109,15 +212,14 @@ session_start();
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="superadmin.php" class="brand-link">
+    <a href="index3.html" class="brand-link">
       <img src="../dist/img/Logo_PLNN.png" alt="PLNLOGO" class="brand-image img-rectangle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">For-Pi</span>
     </a>
 
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user (optional) -->
+      <!-- Sidebar -->
+      <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
@@ -141,8 +243,8 @@ session_start();
         </div>
       </div>
 
-     <!-- Sidebar Menu -->
-     <nav class="mt-2">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
@@ -157,15 +259,6 @@ session_start();
           </li>
 
           <li class="nav-item menu-open">
-            <a href="dashboard-adminpusat.php" class="nav-link">
-              <i class="nav-icon fas fa-dashboard"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
-
-          <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -175,20 +268,20 @@ session_start();
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
+              <li class="nav-item menu-open">
                 <a href="pemilik-adminpusat.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Pemilik</p>
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item menu-open">
                 <a href="jabatan-adminpusat.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Jabatan</p>
                 </a>
               </li>
 
-              <li class="nav-item">
+              <li class="nav-item menu-open">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -261,28 +354,10 @@ session_start();
     <!-- /.sidebar -->
   </aside>
 
-
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Dashboard Periode Perencanaan</h1>
 
-          </div>
-          <div class="col-sm-6">
-          <a href="create-adminpusat.php" class="btn-create">New Data+</a>
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="admin-pusat.php">Home</a></li>
-              <li class="breadcrumb-item active">Juknis</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
 
     <!-- Main content -->
     <section class="content">
@@ -291,108 +366,53 @@ session_start();
           <div class="col-12">
 
             <div class="card">
-
+              <div class="card-header mx-auto">
+                <h1>Edit Tipe Target</h1>
+              </div>
               <!-- /.card-header -->
-              <div class="card-body text-center">
-            <form action="connect-dashboard-adminpusat.php" method="post">
-                  <div class="row mb-3">
-                  <label for="password" class="col-sm-2 col-form-label">Divisi</label>
-                  <div class="col-sm-10">
-                  <select class="form-control" aria-label="Default select example" name="divisi">
-                  <option selected disabled>Divisi</option>
-                  <?php
-                  $records = mysqli_query($koneksi, "SELECT * FROM tb_pemilik");
-                  while($data = mysqli_fetch_array($records)){
-                    echo "<option value='".$data['pemilik']."'>".$data['pemilik']."</option>";
-                  }
-                  ?>
-                </select>
-                    </div>
-                  </div>
+              <div class="card-body">
+                <!--Input Data-->
+        <form method="POST">
 
-                    <div class="row mb-3">
-                    <label for="" class="col-sm-2 col-form-label">Upload Data</label>
-                    <div class="col-sm-10">
-                      <input type="number" class="form-control" name="upload_data">
-                    </div>
-                  </div>
 
-                    <div class="row mb-3">
-                    <label for="" class="col-sm-2 col-form-label">Upload KPI</label>
-                    <div class="col-sm-10">
-                      <input type="number" class="form-control" name="upload_kpi">
-                    </div>
-                  </div>
+  <div class="row mb-3">
+    <label for="" class="col-sm-2 col-form-label">Upload Data</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="upload_data" value="<?= $vupload_data ?>">
+    </div>
+  </div>
 
-                    <div class="row mb-3">
-                    <label for="" class="col-sm-2 col-form-label">Inspirasi KPI</label>
-                    <div class="col-sm-10">
-                      <input type="number" class="form-control" name="inspirasi_kpi">
-                    </div>
-                  </div>
+  <div class="row mb-3">
+    <label for="" class="col-sm-2 col-form-label">Upload KPI</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="upload_kpi" value="<?= $vupload_kpi ?>">
+    </div>
+  </div>
 
-                    <div class="row mb-3">
-                    <label for="" class="col-sm-2 col-form-label">Approval</label>
-                    <div class="col-sm-10">
-                      <input type="number" class="form-control" name="approval">
-                    </div>
-                  </div>
-                  <input type="submit" class="btn btn-primary btn-register">
+  <div class="row mb-3">
+    <label for="" class="col-sm-2 col-form-label">Inspirasi KPI</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="inspirasi_kpi" value="<?= $vinspirasi_kpi ?>">
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <label for="" class="col-sm-2 col-form-label">Approval</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="approval" value="<?= $vapproval ?>">
+    </div>
+  </div>
+
+  <div class="text-center">
+      <hr>
+      <button class="btn btn-success" name="btn-simpan" type="submit">Save</button>
+     </div>
+</form>
+        </div>
+     <!--Akhir input data-->
+        </div>
+     <!--Akhir input data-->
               </div>
-              </form>
-
-
-              <div class="card-body text-center">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                  <th>Unit/Divisi</th>
-                  <th>Upload Data</th>
-                  <th>Upload KPI</th>
-                  <th>Inspirasi KPI</th>
-                  <th>Approval</th>
-                  <th>Status</th>
-                  <th>Aksi</th>
-                  </tr>
-                  </thead>
-                  
-                  <tbody>
-                    <?php 
-                       $user = mysqli_query($koneksi, "SELECT * FROM tb_dashboard order by id asc");
-                       while($data = mysqli_fetch_array($user)) :
-                    ?>
-                  <tr>
-                    <td><?= $data['divisi'] ?></td>
-                    <td><?= $data['upload_data'] ?></td>
-                    <td><?= $data['upload_kpi'] ?></td>
-                    <td><?= $data['inspirasi_kpi'] ?></td>
-                    <td><?= $data['approval'] ?></td>
-                    <?php
-                    $hitung = ($data['upload_data'] + $data['upload_kpi'] + $data['inspirasi_kpi'] + $data['approval']) / 4;
-                    $total = $hitung;
-                    if($total == 100){
-                      echo '<td>'. '<button class="btn btn-success">' . 'Selesai' .'</button>' .'</td>';
-                    }
-                    else if($total >= 50 && $total <= 100){
-                      echo '<td>'. '<button class="btn btn-warning">' . 'Segera Selesai' . '</button>' . '</td>';
-                    }
-
-                    else if($total <= 50){
-                      echo '<td>'. '<button class="btn btn-danger">' . 'Belum Selesai' . '</button>' . '</td>';
-                    }
-                  
-                    else{
-                      echo '<td>'.'</td>';
-                    }
-                    ?>
-                    <td>
-                      <a href="editdashboard-adminpusat.php?hal=edit&id=<?=$data['id']?>" class="btn btn-warning">Edit</a>
-                    </td>
-                  <?php endwhile; ?>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
@@ -400,18 +420,12 @@ session_start();
           <!-- /.col -->
         </div>
         <!-- /.row -->
-      </div>
+
       <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>PLN Kantor Pusat</b>
-    </div>
-    <strong>Copyright &copy; 2022 <a href="#">PT PLN (PERSERO)</a>.</strong> All rights reserved.
-  </footer>
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
@@ -440,22 +454,6 @@ session_start();
 <script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- AdminLTE App -->
 <script src="../dist/js/adminlte.min.js"></script>
-<!-- Page specific script -->
-<script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-</script>
+
 </body>
 </html>
